41d088178388cc65b34f4b192cd9c313f02f7373
Thomas Müller <thomas.mueller@tmit.eu>
2016-05-06 11:06:02 +0200

    Update system_setup.sh to install 12.1

--- system_setup.py
+++ system_setup.py
@@ -30,14 +30,12 @@ class SystemSetup(object):
 			'1. Obtain sqlnet.ora, tnsnames.ora, and possibly ldap.ora \n'
 			'   from your DBA.\n'
 			'2. Place these files into the following directory: \n'
-			'   /usr/lib/oracle/11.2/client64/network/admin\n'
+			'   /usr/lib/oracle/12.1/client64/network/admin\n'
 			'3. Restart your terminal to load the new environment variables.\n'
 			'4. Attempt to connect to your database with sqlplus64.\n'
 			'5. Star this repo on Github so that I know if it is being used.\n'
 			'6. Submit any issues on Github(http://bit.ly/XoqtcH).\n'
 			'###############################################################\n')
-
-		raw_input()
 
 	def install_alien(self):
 		subprocess.check_call(["apt-get", "-y", "install", "alien"])
@@ -50,21 +48,21 @@ class SystemSetup(object):
 		subprocess.check_call(
 			['alien',
 			'-iv',
-			'oracle_rpms/oracle-instantclient11.2-basic-11.2.0.3.0-1.x86_64.rpm'])
+			'oracle_rpms/oracle-instantclient12.1-basic-12.1.0.2.0-1.x86_64.rpm'])
 
 		subprocess.check_call(
 			['alien',
 			'-iv',
-			'oracle_rpms/oracle-instantclient11.2-sqlplus-11.2.0.3.0-1.x86_64.rpm'])
+			'oracle_rpms/oracle-instantclient12.1-sqlplus-12.1.0.2.0-1.x86_64.rpm'])
 
 		subprocess.check_call(
 			['alien',
 			'-iv',
-			'oracle_rpms/oracle-instantclient11.2-devel-11.2.0.3.0-1.x86_64.rpm'])
+			'oracle_rpms/oracle-instantclient12.1-devel-12.1.0.2.0-1.x86_64.rpm'])
 
 		oracle_configuration_file = open('/etc/ld.so.conf.d/oracle.conf', 'w')
 		oracle_configuration_file.writelines(self.file_insert_header)
-		oracle_configuration_file.write('/usr/lib/oracle/11.2/client64/lib\n')
+		oracle_configuration_file.write('/usr/lib/oracle/12.1/client64/lib\n')
 		oracle_configuration_file.close()
 
 		subprocess.check_call(["sudo", "ldconfig"])
@@ -72,22 +70,22 @@ class SystemSetup(object):
 		system_environment_vars = open('/etc/profile.d/oracle.sh', 'w')
 		system_environment_vars.writelines(self.file_insert_header)
 		system_environment_vars.write(
-			'export ORACLE_HOME=/usr/lib/oracle/11.2/client64\n')
+			'export ORACLE_HOME=/usr/lib/oracle/12.1/client64\n')
 		system_environment_vars.write(
-			'export TNS_ADMIN=/usr/lib/oracle/11.2/client64/network/admin\n')
+			'export TNS_ADMIN=/usr/lib/oracle/12.1/client64/network/admin\n')
 		system_environment_vars.close()
 
-		os.makedirs('/usr/lib/oracle/11.2/client64/network/admin')
+		os.makedirs('/usr/lib/oracle/12.1/client64/network/admin')
 
 		home_directory = os.environ['HOME']
 		bash_profile = open(home_directory + '/.bashrc', 'a')
 		bash_profile.writelines(self.file_insert_header)
 		bash_profile.write(
-			'export LD_LIBRARY_PATH=/usr/lib/oracle/11.2/client64/lib\n')
+			'export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib\n')
 		bash_profile.write(
-			'export ORACLE_HOME=/usr/lib/oracle/11.2/client64\n')
+			'export ORACLE_HOME=/usr/lib/oracle/12.1/client64\n')
 		bash_profile.write(
-			'export TNS_ADMIN=/usr/lib/oracle/11.2/client64/network/admin\n')
+			'export TNS_ADMIN=/usr/lib/oracle/12.1/client64/network/admin\n')
 		bash_profile.close()
 
 if __name__ == '__main__':
